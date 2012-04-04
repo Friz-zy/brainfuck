2f4981835263c6196be4d068cdd34b2873ccce2b
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2012-04-04 22:48:48 +0300

    second commit

---
+++ brainfuck.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# write by Filipp Frizzy <filipp.s.frizzy@gmail.com> and google translate
+
+import brain
+
+n = input('write the number of bits ')
+
+code = """++++++++++[>+++++++>++++++++++>++++++++++>++++++++++>+++++++++++>+++>++++++++>+++
+ ++++++++>+++++++++++>++++++++++>++++++++++>+++<<<<<<<<<<<< - ]>++.>+.>++++++++.>++++++++.>+.>++.>+++++++.>+.>++++.>++++++++.>.>+++."""#input('write the code, bitch! ') # take program body
+
+out = brain.brain(n, code) 
+print(out)	 # output resalt
+      
+  
